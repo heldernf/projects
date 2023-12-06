5855b2a2de2b733f6f4cd95c37779f1c297290da
Helder Neto <120188176+heldernf@users.noreply.github.com>
2023-12-05 22:50:52 -0300

    .

--- landing-page-projects/model-002/script.js
+++
@@ -1,9 +0,0 @@
-function menuDrop() {
-    const ulMenu = document.getElementById('ulMenu')
-    
-    if (ulMenu.style.transform == 'scale(1)') {
-        ulMenu.style.transform = 'scale(0)'
-    } else {
-        ulMenu.style.transform = 'scale(1)'
-    }
-}
