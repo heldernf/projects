12561c00dc02e958e913504becd5248056d63af6
Helder Neto <helderneto2005@gmail.com>
2024-02-23 22:42:01 -0300

    portfolio

--- portfolio-1.0/script.js
+++ portfolio-1.0/script.js
@@ -1,1 +1,10 @@
-console.log("olá");
+const menu = document.getElementById("menu-trigger");
+const options = document.getElementById("menu-options");
+
+menu.addEventListener('click', function() {
+    if (options.style.transform == "translateX(0%)") {
+        options.style.transform = "translateX(100%)";
+    } else {
+        options.style.transform = "translateX(0%)";
+    }
+});
