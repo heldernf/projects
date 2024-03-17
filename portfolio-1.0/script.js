b9fba0588d1e4a1e37a8c77fe8275e9072471f2b
Helder Neto <helderneto2005@gmail.com>
2024-03-17 19:50:22 -0300

    Projeto 100% finalizado

--- portfolio-1.0/script.js
+++ portfolio-1.0/script.js
@@ -8,9 +8,9 @@ const sections = document.querySelectorAll("section");
 // NÃO DEIXA O MENU BUGAR SE ELE ESTIVER ABERTO E OCORRER UM resize para 600px e vice e verso
 window.addEventListener('resize', function () {
     if (window.innerWidth == 600) {
-        menuIcon.classList.remove('animeMenu')
         optionsBox.classList.remove('visible')
         optionsBox.classList.add('invisible')
+        menuIcon.classList.remove('animeMenu')
     }
 });
 
@@ -58,7 +58,6 @@ main.addEventListener('click', function () {
 
 // ATUALIZA O LINK DO MENU QUE ESTÁ ATIVO
 let timeout;
-
 function attOption() {
     clearTimeout(timeout);
     timeout = setTimeout(function () {
@@ -74,70 +73,70 @@ function attOption() {
 }
 window.addEventListener('scroll', attOption);
 
-// PASSAR IMAGENS AUTOMATICAMENTE (SLIDEPT1)
+// SLIDE FUNCTIONS ---- SLIDE FUNCTIONS ---- SLIDE FUNCTIONS
 const firstImg = document.querySelectorAll(".first-img-slide");
 const btnSlide = document.querySelectorAll(".slide-button");
-const totalImg = 12;
-let pSlide = 1;
+const slideBox = document.getElementsByClassName("slide-box");
+const totalImg = slideBox.length * slideBox[0].getElementsByTagName("img").length;
+let positionSlide = 1;
 
 function activeBackground() {
-    for (let i = 0; i < totalImg; i++) {
-        btnSlide[i].style.backgroundColor = "transparent";
-    }
-
-    if (pSlide == 0) {
-        btnSlide[0].style.backgroundColor = "white"
-        btnSlide[3].style.backgroundColor = "white"
-        btnSlide[6].style.backgroundColor = "white"
-        btnSlide[9].style.backgroundColor = "white"
-    } else if (pSlide == 1) {
-        btnSlide[1].style.backgroundColor = "white"
-        btnSlide[4].style.backgroundColor = "white"
-        btnSlide[7].style.backgroundColor = "white"
-        btnSlide[10].style.backgroundColor = "white"
-    } else if (pSlide == 2) {
-        btnSlide[2].style.backgroundColor = "white"
-        btnSlide[5].style.backgroundColor = "white"
-        btnSlide[8].style.backgroundColor = "white"
-        btnSlide[11].style.backgroundColor = "white"
+    btnSlide.forEach(btn => {
+        btn.style.backgroundColor = "transparent";
+    });
+    for (let i = positionSlide; i < totalImg; i += 3) {
+        btnSlide[i].style.backgroundColor = "white";
     }
 }
 
+// PASSA IMAGENS AUTOMATICAMENTE
 function moveSlide() {
-    switch (pSlide) {
+    switch (positionSlide) {
         case 0:
             firstImg.forEach(img => {
                 img.style.marginLeft = "0%";
             })
             activeBackground();
-            pSlide++;
+            positionSlide++;
             break;
         case 1:
             firstImg.forEach(img => {
                 img.style.marginLeft = "-100%";
             })
             activeBackground();
-            pSlide++;
+            positionSlide++;
             break;
         case 2:
             firstImg.forEach(img => {
                 img.style.marginLeft = "-200%";
             })
             activeBackground();
-            pSlide = 0;
+            positionSlide = 0;
             break;
     }
 }
+const intervalo = setInterval(moveSlide, 10000);
 
-setInterval(function () {
-    moveSlide();
-}, 3000)
-
-// PASSAR IMAGENS DO SLIDE CLeICANDO (SLIDE PT2)
+// PASSA IMAGENS DO SLIDE CLICANDO
 btnSlide.forEach((button, index) => {
-    button.addEventListener('click', function () {
-        pSlide = index % 3;
-        moveSlide();
-        console.log(index, pSlide)
+    button.addEventListener('click', function () {         
+        const img = button.parentNode.parentNode.getElementsByClassName("first-img-slide");
+        const slideControls = button.parentNode.querySelectorAll(".slide-button");
+        const positionSlideDois = index % 3;
+
+        slideControls.forEach(control => {
+            control.style.backgroundColor = "transparent"
+        });
+
+        if (positionSlideDois == 0) {
+            img[0].style.marginLeft = "0%";
+            btnSlide[index].style.backgroundColor = "white";
+        } else if (positionSlideDois == 1) {
+            img[0].style.marginLeft = "-100%";
+            btnSlide[index].style.backgroundColor = "white";
+        } else {
+            img[0].style.marginLeft = "-200%";
+            btnSlide[index].style.backgroundColor = "white";
+        }
     });
 });
