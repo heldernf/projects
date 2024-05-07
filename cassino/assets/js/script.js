e159aea03f1979683129ce4791d5787a84e5e16c
Helder Neto <helderneto2005@gmail.com>
2024-05-06 23:34:10 -0300

    Fix

--- cassino/assets/js/script.js
+++ cassino/assets/js/script.js
@@ -1,5 +1,9 @@
 const navTrigger = document.getElementById("navTrigger");
 const nav = document.getElementById("nav");
+
+if (window.innerWidth > 1200) {
+    nav.style.translate = "0%";
+}
 function navToggle() {
     if (getComputedStyle(nav).translate == "-100%") {
         nav.style.translate = "0%";
@@ -9,18 +13,17 @@ function navToggle() {
 }
 navTrigger.addEventListener("click", navToggle);
 
-
 const h2Nav = document.querySelectorAll(".h2Nav");
 function h2ListToggle(nextSibling) {
     if (getComputedStyle(nextSibling).display !== "none") {
         nextSibling.style.display = "none";
     } else {
-        nextSibling.style.display = nextSibling.display;
+        nextSibling.style.display = nextSibling.originalDisplay;
     }
 }
 h2Nav.forEach(h2 => {
     const nextSibling = h2.nextElementSibling;
-    nextSibling.display = getComputedStyle(nextSibling).display;
+    nextSibling.originalDisplay = getComputedStyle(nextSibling).display;
     
     h2.addEventListener("click", () => {
         h2ListToggle(nextSibling);
